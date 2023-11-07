097a6b66d18c982e32bb7d7c5217057e6b55a64d
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2023-11-07 15:44:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,35 @@
 import streamlit as st
-st.write("hello")
- 
-st.title("东临碣石聊天室")
- 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
- 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
- 
-# React to user input
-if prompt := st.chat_input("What's your message?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    
-    response = f"答复: {prompt}"
-    # Display assistant response in chat message container
- 
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+st.title("深圳翩翩")
+
+#申请的api_key
+openai.api_key = "xxxxxxxxxxxxxxxxx" 
+def generate_response(prompt):
+    completion=openai.Completion.create(
+        model='text-davinci-003',
+        prompt=prompt,
+        max_tokens=1024,
+        temperature=0.6
+    )
+    message=completion.choices[0].text
+    return message
+
+st.markdown("#### 我是ChatGPT聊天机器人,我可以回答您的任何问题")
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+user_input=st.text_input("请输入您的问题:",key='input')
+if user_input:
+    output=generate_response(user_input)
+    st.session_state['past'].append(user_input)
+    st.session_state['generated'].append(output)
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], 
+                is_user=True, 
+                key=str(i)+'_user')

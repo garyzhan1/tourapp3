ca469e25e50b39baebcd3e61d19019d6ed789746
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2023-11-07 15:34:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,26 @@
 import streamlit as st
 st.write("hello")
+ 
+st.title("东临碣石聊天室")
+ 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+ 
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+ 
+# React to user input
+if prompt := st.chat_input("What's your message?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    response = f"答复: {prompt}"
+    # Display assistant response in chat message container
+ 
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history

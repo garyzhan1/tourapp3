d254661fca2e950f78d953d4349c879b0eeef680
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2023-12-02 14:30:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,24 @@
 import streamlit as st
 import pandas as pd
 import csv
-
-def read_csv('supermarkt_sales.csv'):
-    """
-    读取csv文件并以列表形式输出
-    :param file_path: str
-    :return: [list[str]]
-    """
-    res = []
-    with open('supermarkt_sales.csv', 'r',encoding='utf-8')as f:
-        read = csv.reader(f)
-        for line in read:
-            res.append(line)
-    return res
+from streamlit_apex_charts import line_chart, bar_chart, pie_chart, area_chart, radar_chart
+ 
+st.set_page_config(page_title="快速基于Dataframe构建数据大屏", layout="wide")
+ 
+file = st.sidebar.file_uploader("请上传csv表格", type=["csv"])
+if file is not None:
+    df1 = pd.read_csv(file, encoding="gbk")
+    column = df1.columns  #获取表头
+    df = pd.DataFrame(df1,columns=column)
+ 
+    line_chart('Line chart',df)
+    c1, c2 = st.columns(2)
+    with c1:
+        bar_chart('Bar chart',df)
+        pie_chart('Pie chart',df)
+    with c2:
+        area_chart('Area chart',df)
+        radar_chart('Radar chart',df)
 
 st.title("深圳翩翩-测试")
  

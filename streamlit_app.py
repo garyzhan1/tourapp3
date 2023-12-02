ecdd5dd1c79eb7a96965aa69a4a21c2b871a9495
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2023-12-02 16:34:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,16 +19,6 @@ st.line_chart(data3)
 st.bar_chart(data3)
 st.area_chart(data3)
 
-
-col1, col2 = st.columns([3, 1])
-data2 = np.random.randn(10, 1)
-
-col1.subheader("A wide column with a chart")
-col1.line_chart(data2)
-
-col2.subheader("A narrow column with the data")
-col2.write(data2)
-
 data_df = pd.DataFrame(
     {
         "Dow Jones trend": [
